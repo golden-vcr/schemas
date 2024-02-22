55ad91f32beff2b9133a5e1f6527865271a76941
Alex Forsythe <awforsythe@gmail.com>
2024-02-22 09:44:19 -0500

    Add 'extra' field to image alert payload

--- onscreen-events/types_image.go
+++ onscreen-events/types_image.go
@@ -9,5 +9,6 @@ type PayloadImage struct {
 	Viewer      core.Viewer       `json:"viewer"`
 	Style       genreq.ImageStyle `json:"style"`
 	Description string            `json:"description"`
+	Extra       string            `json:"extra"`
 	ImageUrl    string            `json:"image_url"`
 }
